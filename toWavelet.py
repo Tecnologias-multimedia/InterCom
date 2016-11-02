dc05fe881eb385797b0335d4234c3afec5db2000
Nikita Panteleev <rorikandrorikids@gmail.com>
2016-11-02 19:05:11 +0100

    Especificaciones sobre el modulo toWywavelts anhadidas

--- toWavelet.py
+++ toWavelet.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Modulo que hace la transformada y separa en planos el sonido raw
+
+Modulo que recibe el sonido raw grabado en intercom
+y lo convierte primero en un array de enteros,
+despues le aplica la transformada y por ultimo
+separa los datos ya transformados por planos de bits/bytes - corregir
+Estos planos son enviados en los paquetes upd para su proxima reconstruccion
+
+FALTA DECIDIR EL TIPO DE TRANSFORMADA A USAR.
+"""
+
 #cambiar los import de numpy y pywt a lo necesario
 import numpy as np
 import pywt as wt
@@ -15,11 +27,11 @@ def arraySecuencial(data):
         frames.append(data[i])
     return frames
 
-
 def main():
-    return 0
+    """modulo de pruebas
 
-if __name__ == '__main__':
+    Aqui van los datos para realizar las pruebas sobre el modulo aislado
+    """
     p = PyAudio()
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
@@ -28,4 +40,6 @@ if __name__ == '__main__':
                     frames_per_buffer=CHUNK)
     data = stream.read(1024)
     arraySecuencial(data)
+
+if __name__ == '__main__':
     main()
