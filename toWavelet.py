8905631b82fec27a1767538197059d25d2e1d996
Nikita Panteleev <np258@inlumine.ual.es>
2016-11-14 11:12:46 +0100

    planos de bits a medias

--- toWavelet.py
+++ toWavelet.py
@@ -3,7 +3,7 @@
 
 Modulo que recibe el sonido raw grabado en intercom
 y lo convierte primero en un array de enteros,
-despues le aplica la transformada y por ultimo
+despues le aplica la transformada (esta ha de ser normalizada a enteros) y por ultimo
 separa los datos ya transformados por planos de bits/bytes - corregir
 Estos planos son enviados en los paquetes upd para su proxima reconstruccion
 
@@ -20,6 +20,7 @@ CHUNK = 1024
 FORMAT = paInt16
 CHANNELS = 1
 RATE = 44100
+VALORES = 32
 
 def arraySecuencial(data):
     frames = []
@@ -27,10 +28,40 @@ def arraySecuencial(data):
         frames.append(data[i])
     return frames
 
+def transform(frames):
+    """realiza la TRANSFORMADA, normaliza y separa los datos en las colecciones
+
+    despues pega las colecciones en una sola"""
+    coeffs = wt.wavedec(frames, 'db1', level=5)
+    transformada = []
+    for i in coeffs:
+        for e in i:
+            transformada.append(int(round(e)))
+    planos = []
+    for plano in range(0, 32):
+        comp = 32-plano
+        planos[plano] = []
+        for v in transformada:
+            temp = (v & (2**comp)) >> comp
+            for i in range(0, 32):
+                if i == 0:
+                    planos[plano][]
+
+
+
+    print(transformada[0], type(transformada[0]))
+    print(bin(transformada[0]))
+    print(transformada[0] & (2**31))
+    print(bin(transformada[0] & (2**31)))
+
+    #aqui sacamos los planos de bits y devolvemos un
+    #array de arrays de planos de bits
+
 def main():
     """modulo de pruebas
 
     Aqui van los datos para realizar las pruebas sobre el modulo aislado
+    Tambien sirve como ejemplo de uso
     """
     p = PyAudio()
     stream = p.open(format=FORMAT,
@@ -40,10 +71,7 @@ def main():
                     frames_per_buffer=CHUNK)
     data = stream.read(1024)
     frames = arraySecuencial(data)
-    coeffs = wt.wavedec(frames, 'db1', level=5)
-    cA5, cD5, cD4, cD3, cD2, cD1 = coeffs
-    print(len(cD1))
-    print(len(frames))
+    transform(frames)
 
 if __name__ == '__main__':
     main()
