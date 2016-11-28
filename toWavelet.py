3e1be7415986a1b99ec634e2c5c628d26bd62b71
Juan Antonio Rodríguez Baeza <juanrdzbaeza@gmail.com>
2016-11-28 09:26:58 +0100

    Probando a deshacer los planos y transpira comprobar si tenemos restos iguales

--- toWavelet.py
+++ toWavelet.py
@@ -29,33 +29,76 @@ def arraySecuencial(data):
     return frames
 
 def transform(frames):
-    """realiza la TRANSFORMADA, normaliza y separa los datos en las colecciones
-
-    despues pega las colecciones en una sola"""
+    """
+    realiza la TRANSFORMADA, normaliza y separa los datos en 
+    las colecciones despues pega las colecciones en una sola
+    """
     coeffs = wt.wavedec(frames, 'db1', level=2)
+    #print(coeffs)
     transformada = []
     for i in coeffs:
         for e in i:
             transformada.append(int(round(e)))
 
+    #print(transformada)
+    #print(len(transformada))
     planos = {}
     for plano in range(0, 32):
         comp = 32-plano
+        
         n = 0
         bloque = 0
         planos[plano] = []
-
-        for v in transformada:
-            temp = ((v & (2**comp)) >> comp)
-            bloque += (temp << (32 - n))
+        #print(planos)
+        #print("caca", plano)
+        for entero in transformada:
+            #print(bin(entero & (2**comp)))
+            temp = ((entero & (2**comp)) >> comp)
+            #print(bin(temp))
+            bloque += (temp << (32 - n)) # cada bloque tendra 32 bits
+            #print(bin(bloque))
             n = n+1
             if n == 31:
+                #print(plano)
                 planos[plano].append(bloque)
+                
                 n = 0
                 bloque = 0
     
-    print(transformada)
-    print(planos)
+
+    print(len(planos))
+    for plane in planos:
+        print("este es el plano nº ",plane)
+        pepe = []
+        posicion = 32-plane
+        m = 0
+        numero = 0
+        for bloke in planos[plane]:
+            pass
+            #print(bin(bloke))
+
+            #binario = ((bloke & (2**posicion)) >> posicion)
+            #print(binario)
+            #numero += (binario << (32 - m))
+            #m = m+1
+            #if m == 31:
+             #   pepe.append(numero)
+              #  m = 0
+               # numero = 0
+            #print(numero)
+
+
+
+        #print("aqui termina un plano")
+
+            #tempo = (y & (2**x) >> x)
+            #print(tempo)
+
+
+
+
+    #print(len(transformada))
+    #print(planos)
 
     #aqui sacamos los planos de bits y devolvemos un
     #array de arrays de planos de bits
@@ -63,8 +106,8 @@ def transform(frames):
 def main():
     """modulo de pruebas
 
-    Aqui van los datos para realizar las pruebas sobre el modulo aislado
-    Tambien sirve como ejemplo de uso
+    Aqui van los datos para realizar las pruebas sobre el 
+    modulo aislado Tambien sirve como ejemplo de uso
     """
     p = PyAudio()
     stream = p.open(format=FORMAT,
@@ -74,8 +117,15 @@ def main():
                     frames_per_buffer=CHUNK)
     data = stream.read(1024)
     #data = [5, 12, 3, 6]
+    #data = [0, 0, 0, 0]
     frames = arraySecuencial(data)
     transform(frames)
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
