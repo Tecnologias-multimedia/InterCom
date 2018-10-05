ebc972797f55816f5b259cc8b324ef4673ede407
GervillaesPhoto <gervillaesphoto@gmail.com>
2018-10-05 12:18:02 +0200

    Cambios en PEP8

--- Issue16/servidor2.py
+++ Issue16/servidor2.py
@@ -1,31 +1,32 @@
-#Libreerias necesarias para recibir audio por udp
+# Librerias necesarias para recibir audio por udp
 import socket
 import pyaudio
 
-#variables del audio para ser lo mejor posible a de ser las mismas que el servidor
+# Variables del audio (se recomienda que sean las mismas que las del servidor)
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 
-#conectamos al que nos envia los datos
+# Conectamos al que nos envia los datos
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 50007))
 
-#Crear variable de pyaudio
+# Crear variable de pyaudio
 p = pyaudio.PyAudio()
-#creamos otra variable para trabajar con pyaudio
+# Creamos otra variable para trabajar con pyaudio
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
-#iniciamos el audio
+# Iniciamos el audio
 stream.start_stream()
+
 
 def main():
     print("*_>recording")
-#ir leyendo el audio miemtras se recibe
+# Se va leyendo el audio miemtras se recibe
     while True:
         try:
             data = stream.read(CHUNK)
@@ -34,7 +35,9 @@ def main():
         s.sendall(data)
 
     print("*_>done recording")
-#cerramos el audio terminamos con la libreria pyaudio y cerramos el trabajo con el puerto
+
+# Cerramos el audio terminamos con la libreria pyaudio
+# Y cerramos el trabajo con el puerto
     stream.stop_stream()
     stream.close()
     p.terminate()
