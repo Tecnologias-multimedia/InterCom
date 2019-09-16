529239d05553cb7df496558ada9e910ac37ab461
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-09-16 21:58:50 +0200

    .

--- test/wire.py
+++ test/wire.py
@@ -4,8 +4,9 @@ few samples and play them back immediately).
 """
 
 import pyaudio
+import sys
 
-CHUNK = 1024
+CHUNK = 4096
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
@@ -28,6 +29,7 @@ print("* recording")
 
 while True:
     data = input.read(CHUNK)
+    sys.stderr.write(" {}".format(data[0])); sys.stderr.flush()
     output.write(data, CHUNK)
 
 print("* done")
