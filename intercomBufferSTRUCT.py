137d88f696e6cd63a854adbe944c37b757214c8b
Gelrok <Toni.Schaarschmidt@hotmail.de>
2019-10-13 20:00:47 +0200

    Cleanup output and comment corrections

--- intercomBufferSTRUCT.py
+++ intercomBufferSTRUCT.py
@@ -9,7 +9,9 @@
 # SOULUTION 1 working solution
 
 #IMPLEMENTATION:
-#Current VERSION 1.7
+#Current VERSION 1.8
+#1.8    -textoutput and code commentes
+
 #1.7    -changed unpack method. Use without numpy.delete/numpy.insert
 
 #1.6    -Added comments for explanation
@@ -28,6 +30,7 @@
 #1.0    -implementation buffer 
 
 import sounddevice as sd 
+#Import Class object "Intercom" from intercom
 from intercom import Intercom
 import numpy                                                                    # https://numpy.org/
 import argparse                                                                 # https://docs.python.org/3/library/argparse.html
@@ -38,15 +41,22 @@ import struct
 
 if __debug__:
     import sys
-    
+
+#Setup child class for Intercom Parent Class
 class IntercomBuffer(Intercom):
 
     def init(self, args):
         Intercom.init(self,args)
         self.buffer_size=args.buffer_size
         self.packet_list=[]
+        #initialize buffer and fill with silence
         for x in range(self.buffer_size):
-            self.packet_list.append([])
+            self.packet_list.append(numpy.zeros(
+                    (self.samples_per_chunk, self.bytes_per_sample),
+                    self.dtype))
+        self.silence=numpy.zeros(
+                    (self.samples_per_chunk, self.bytes_per_sample),
+                    self.dtype)
         self.packet_send=0
 
         #Benchmark Variables
@@ -54,7 +64,7 @@ class IntercomBuffer(Intercom):
         self.cpu_max=0
         self.cycle=0       #caculation cycle for average value
         
-        #first index -1 for delaying play
+        #first index -1 for delaying start of playing
         self.packet_received=-1
         
         #calc size of message example: s. per chunk = 1024 & channel=2 (1024*2)
@@ -75,10 +85,9 @@ class IntercomBuffer(Intercom):
 
             #define message array for unpacking
             message=[]
-            #unpack index to index and message to array at pointer *message
-            idx, *message=struct.unpack('<H{}h'.format(self.msgsize),messagepack) #unpack structure
-            self.packet_list[idx % self.buffer_size]=message    #out[0]=index, out=index & message
-            sys.stderr.write("\nIDX: {} - MSG: {}".format(idx,len(message)));sys.stderr.flush()
+            #unpack index to idx and message to array at pointer *message
+            idx, *message=struct.unpack('!H{}h'.format(self.msgsize),messagepack) #unpack structure
+            self.packet_list[idx % self.buffer_size]=message    
 
             #--------------Benchmark-------------------------
             self.cycle+=1
@@ -92,75 +101,101 @@ class IntercomBuffer(Intercom):
             #    (self.cpu_load/self.cycle),
             #    self.cpu_max)); sys.stderr.flush();
             #--------------Benchmark END---------------------
-            
-        def record_send_and_play(indata, outdata, frames, time, status):
-            #get message from stream and write to data array
+
+        def record_send(indata, outdata, frames, time, status):
+            #put incoming sound to 1D array
             data=numpy.frombuffer(
                 indata,
                 numpy.int16)
-            #Put index and data in structure and define "little-endian" format with size of messages and index ("<")
+
+            #Put index (packet_send) and data in structure and define "network-packet" format with size of messages and index ("!")
             #H = unsigned short integer (0 - 65535) for index (2^16), h = signed short integer (-32768 - 32767)
-            #example: 1024 samples, 2 cahnnels =  '<H2048h'
-            datapack=struct.pack('<H{}h'.format(self.msgsize),self.packet_send,*data)
+            #example: 1024 samples, 2 channels =  '!H2048h'
+            datapack=struct.pack('!H{}h'.format(self.msgsize),self.packet_send,*data)
+            sending_sock.sendto( 
+                datapack,
+                (self.destination_IP_addr, self.destination_port))
+
+            #calc next index to send, if bigger than 65535 reset to zero
+            self.packet_send=(self.packet_send+1)%(2**16)
+
+            #Check if we received BUFFER FINISHED point.
+            if self.packet_received<0:
+                nonzeros=[]           
+
+                #Collect index of all non-silence elements in buffer
+                for s in range(self.buffer_size):
+                    if numpy.array_equal(self.packet_list[s],self.silence)==False:
+                        nonzeros.append(s)
+                if len(nonzeros)>0:
+                    sys.stderr.write("B"); sys.stderr.flush()
+                    #print("NONZEROS: {} MAX: {}, MIN {}".format(nonzeros,max(nonzeros),min(nonzeros)))
+                    if max(nonzeros)-min(nonzeros) >= (math.ceil(self.buffer_size/2)-1):
+                        #Set first index of message for playing
+                        self.packet_received=nonzeros[0]
+                        print("\nBUFFERING FINISHED")
+            if __debug__:
+                sys.stderr.write("."); sys.stderr.flush()
+                
+        def record_send_and_play(indata, outdata, frames, time, status):
+            #put incoming sound to 1D array
+            data=numpy.frombuffer(
+                indata,
+                numpy.int16)
+            #Put index (packet_send) and data in structure and define "network-packet" format with size of messages and index ("!")
+            #H = unsigned short integer (0 - 65535) for index (2^16), h = signed short integer (-32768 - 32767)
+            #example: 1024 samples, 2 channels =  '!H2048h'
+            datapack=struct.pack('!H{}h'.format(self.msgsize),self.packet_send,*data)
             
             sending_sock.sendto( 
                 datapack,
                 (self.destination_IP_addr, self.destination_port))
 
-            #calc next index, if bigger than 65535 reset to zero
+            #calc next index to send, if bigger than 65535 reset to zero
             self.packet_send=(self.packet_send+1)%(2**16)
 
-            #check non-zero elements in buffer for delaying playback
-            if self.packet_received<0:
-                nonzeros=[]                                     
-                for s in range(self.buffer_size):
-                    #has to be <= 1 because size of empty message & index is 1
-                    if len(self.packet_list[s])<=1:
-                        nonzeros.append(s)
+            #load next message to play from buffer
+            message=self.packet_list[self.packet_received]
 
-            #if buffer is half filled and we havent started playing (packet_received < 0)
-            if self.packet_received<0 and len(nonzeros)>0:
-
-                #if distnace between lowest received package and highest on is higher than half-size of buffer, bypass and start playing
-                if max(nonzeros)-min(nonzeros) >= math.ceil(self.buffer_size/2): 
-
-                    #get buffer-index of lowest received package
-                    self.packet_received=nonzeros[0] 
-                    print("\nBUFFERING FINISHED - START PLAYING")
-
-            try:
-                message=self.packet_list[self.packet_received]
-                #if message is empty (no package in buffer[index]) raise error (override message with silence)
-                if len(message)<=0:
-                    if len(message)<=0:
-                        raise ValueError
-    
-            except ValueError:
-                message = numpy.zeros(
+            #overwrite index-position of loaded message with silence
+            self.packet_list[self.packet_received]=numpy.zeros(
                     (self.samples_per_chunk, self.bytes_per_sample),
                     self.dtype)
 
-            #if we started playing (after buffer was filled more than half-size) increase received counter
-            if self.packet_received>=0:             
-                self.packet_received=(self.packet_received+1)%self.buffer_size
-                
+            #calc next index to play (number 0 to max of buffer size)
+            self.packet_received=(self.packet_received+1)%self.buffer_size
+
+            #play message
             outdata[:] = numpy.reshape(message,(
                     self.samples_per_chunk, self.number_of_channels))
 
             if __debug__:
                 sys.stderr.write("."); sys.stderr.flush()
 
+        #Only receive until buffer filled to half
+        with sd.Stream(
+                samplerate=self.samples_per_second,
+                blocksize=self.samples_per_chunk,
+                dtype=self.dtype,
+                channels=self.number_of_channels,
+                callback=record_send):
+            print('-=- Press <CTRL> + <C> to quit -=-')
+
+            while self.packet_received<0:
+                receive_and_buffer()
+
+        print('\n-=- START PLAYING -=-')
+        #Receive and play
         with sd.Stream(
                 samplerate=self.samples_per_second,
                 blocksize=self.samples_per_chunk,
                 dtype=self.dtype,
                 channels=self.number_of_channels,
                 callback=record_send_and_play):
-            print('-=- Press <CTRL> + <C> to quit -=-')
             while True:
                 receive_and_buffer()
 
-    
+    #Add new argumente to parser for buffer size
     def parse_args(self):
         parser = Intercom.add_args(self)
 
@@ -171,8 +206,8 @@ class IntercomBuffer(Intercom):
                             type=int,
                             default=4)
 
-        #overwrite default chunk-size by 1024 if no argument is given (system specific)
-        parser.set_defaults(samples_per_chunk=1024)
+        #overwrite default chunk-size by 512 if no argument is given (system specific)
+        parser.set_defaults(samples_per_chunk=512)
         args = parser.parse_args()
         return args
     
