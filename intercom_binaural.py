b1287e88e1c5a3914d38401c270098ebbbb13da2
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-11-04 19:21:14 +0100

    .

--- intercom_binaural.py
+++ intercom_binaural.py
@@ -12,7 +12,12 @@ if __debug__:
 
 class Intercom_binaural(Intercom_bitplanes):
 
-    def record_send_and_play(self, indata, outdata, frames, time, status):
+    def init(self, args):
+        Intercom_bitplanes.init(self, args)
+        if self.number_of_channels == 2:
+            self.record_send_and_play = self.record_send_and_play_stereo
+
+    def record_send_and_play_stereo(self, indata, outdata, frames, time, status):
         pass
 
 if __name__ == "__main__":
