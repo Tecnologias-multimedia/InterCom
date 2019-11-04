bec32c0c74442c38890845daff4b877d7d109935
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-11-04 18:22:42 +0100

    .

--- intercom_bitplanes.py
+++ intercom_bitplanes.py
@@ -3,6 +3,7 @@
 import sounddevice as sd
 import numpy as np
 import struct
+from intercom import Intercom
 from intercom_buffer import Intercom_buffer
 
 if __debug__:
@@ -12,7 +13,32 @@ class Intercom_bitplanes(Intercom_buffer):
 
     def init(self, args):
         Intercom_buffer.init(self, args)
-        self.packet_format = "!HB{self.frames_per_chunk//8}B"
+        self.packet_format = f"!HB{self.frames_per_chunk//8}B"
+
+    def receive_and_buffer(self):
+        message, source_address = self.receiving_sock.recvfrom(Intercom.MAX_MESSAGE_SIZE)
+        chunk_number, bitplane_number, *bitplane = struct.unpack(self.packet_format, message)
+        bitplane = np.asarray(bitplane, dtype=np.uint8)
+        bitplane = np.unpackbits(bitplane)
+        bitplane = bitplane.astype(np.int16)
+        self._buffer[chunk_number % self.cells_in_buffer][:, bitplane_number%self.number_of_channels] |= (bitplane << bitplane_number//self.number_of_channels)
+        return chunk_number
+
+    def record_send_and_play(self, indata, outdata, frames, time, status):
+        noc = self.number_of_channels
+        for bitplane_number in range(noc*16-1, -1, -1):
+            bitplane = (indata[:, bitplane_number%noc] >> bitplane_number//noc) & 1
+            bitplane = bitplane.astype(np.uint8)
+            bitplane = np.packbits(bitplane)
+            message = struct.pack(self.packet_format, self.recorded_chunk_number, bitplane_number, *bitplane)
+            self.sending_sock.sendto(message, (self.destination_IP_addr, self.destination_port))
+        self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
+        chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
+        self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
+        self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
+        outdata[:] = chunk
+        if __debug__:
+            sys.stderr.write("."); sys.stderr.flush()
 
 if __name__ == "__main__":
     intercom = Intercom_bitplanes()
