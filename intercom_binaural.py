a92370723a62c19749b6fc790784d1817a0d0899
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-11-05 10:39:37 +0100

    .

--- intercom_binaural.py
+++ intercom_binaural.py
@@ -2,9 +2,6 @@
 
 import sounddevice as sd
 import numpy as np
-import struct
-from intercom import Intercom
-from intercom_buffer import Intercom_buffer
 from intercom_bitplanes import Intercom_bitplanes
 
 if __debug__:
