828933ce4d745255bb53ec768f60628954780341
Vicente González Ruiz <vicente-gonzalez-ruiz@gmail.com>
2020-01-14 09:44:54 +0100

    .

--- test/pywavelets/subband_gains.py
+++ test/pywavelets/subband_gains.py
@@ -47,18 +47,21 @@ axs[number_of_samples//skip//2].set_ylabel('amplitude')
 
 print("Coefficient\t   Energy")
 
+
+zeros = np.zeros(number_of_samples)
+coeffs = wt.wavedec(zeros, wavelet=wavelet, level=levels, mode=padding)
+arr, coeff_slices = wt.coeffs_to_array(coeffs)
 for i in range(0,number_of_samples,skip):
     
     # The basis functions are created by computing the inverse DWt of
     # an DWT spectrum where all coefficients are zero except one of
     # them. Depending on the position of such coefficient, a different
     # basis function is obtained.
-    zeros = np.zeros(number_of_samples)
-    coeffs = wt.wavedec(zeros, wavelet=wavelet, level=levels, mode=padding)
-    arr, coeff_slices = wt.coeffs_to_array(coeffs)
-    arr[i] = number_of_samples # i is the coeff different of 0
+    
+    arr[i] = number_of_samples # i is the coeff different from 0
     coeffs_from_arr = wt.array_to_coeffs(arr, coeff_slices, output_format="wavedec")
     samples = wt.waverec(coeffs_from_arr, wavelet=wavelet, mode=padding)
+    arr[i] = 0
     print("       %4d" % i, "\t", "%8.2f" % energy(samples))
     axs[i//skip].plot(sample, samples)
 
