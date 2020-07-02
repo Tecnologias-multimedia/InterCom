f0fecdd457441808a12f08a2bb14d8d2f4ab20ac
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-07-02 09:49:46 +0200

    .

--- intercom_bitplanes.py
+++ intercom_bitplanes.py
@@ -28,9 +28,8 @@ class Intercom_bitplanes(Intercom_buffer):
     def init(self, args):
         Intercom_buffer.init(self, args)
         self.packet_format = f"!HB{self.frames_per_chunk//8}B" # Quitar
-        self.precision_bits = 16
-        self.precision_type = np.int16 # Quitar
-        self.number_of_bitplanes_to_send = self.precision_bits * self.number_of_channels
+        self.number_of_bitplanes = 16
+        self.number_of_bitplanes_to_send = self.number_of_bitplanes * self.number_of_channels
         if __debug__:
             self.sent_messages_counter = Value('i', 0)
             self.received_messages_counter = Value('i', 0)
@@ -50,7 +49,7 @@ class Intercom_bitplanes(Intercom_buffer):
         received_chunk_number, received_bitplane_number, *bitplane = struct.unpack(self.packet_format, message)
         bitplane = np.asarray(bitplane, dtype=np.uint8)
         bitplane = np.unpackbits(bitplane)
-        bitplane = bitplane.astype(self.precision_type)
+        bitplane = bitplane.astype(self.sample_type)
         self._buffer[received_chunk_number % self.cells_in_buffer][:, received_bitplane_number % self.number_of_channels] |= (bitplane << received_bitplane_number//self.number_of_channels)
         return received_chunk_number
 
@@ -65,21 +64,23 @@ class Intercom_bitplanes(Intercom_buffer):
 
     # Sends the last recorded chunk (indata).
     def send(self, indata):
-        last_bitplane_to_send = self.precision_bits*self.number_of_channels - self.number_of_bitplanes_to_send
-        for bitplane_number in range(self.precision_bits*2-1, last_bitplane_to_send, -1):
+        last_bitplane_to_send = self.number_of_bitplanes*self.number_of_channels - self.number_of_bitplanes_to_send
+        for bitplane_number in range(self.number_of_bitplanes*2-1, last_bitplane_to_send, -1):
             self.send_bitplane(indata, bitplane_number)
         self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
 
-    def send_message(self, message):
-        super().send_message(message)
-        self.sent_messages_counter.value += 1
-        self.sent_bytes_counter.value += len(message)
+    def send_message(self, chunk):
+        super().send(chunk)
+        if __debug__:
+            self.sent_messages_counter.value += 1
+            self.sent_bytes_counter.value += len(message)
 
     def receive_message(self):
-        message, source_address = super().receive_message()
-        self.received_messages_counter.value += 1
-        self.received_bytes_counter.value += len(message)
-        return message, source_address
+        chunk, sender = super().receive()
+        if __debug__:
+            self.received_chunks_counter.value += 1
+            self.received_bytes_counter.value += len(message)
+        return chunk, sender
 
     def feedback(self):
         old_time = time.time()
