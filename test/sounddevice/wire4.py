4b962e5389b7869939798c64dddcc0e2fe8d5083
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-09-05 11:37:45 +0200

    .

--- test/sounddevice/wire4.py
+++ test/sounddevice/wire4.py
@@ -1,8 +1,9 @@
 import sounddevice as sd
+import numpy as np
 
 CHUNK_SIZE = 1024
 
-stream = sd.RawStream(samplerate=44100, channels=2, dtype='int16')
+stream = sd.Stream(samplerate=44100, channels=2, dtype='int16')
 stream.start()
 while True:
     chunk, overflowed = stream.read(CHUNK_SIZE)
--- test/sounddevice/wire5.py
+++ test/sounddevice/wire5.py
@@ -1,11 +1,15 @@
 import sounddevice as sd
+import numpy as np
 
 CHUNK_SIZE = 1024
 
-stream = sd.RawStream(samplerate=44100, channels=2, dtype='int16')
+stream = sd.Stream(samplerate=44100, channels=2, dtype='int16')
 stream.start()
 while True:
     chunk, overflowed = stream.read(CHUNK_SIZE)
     if overflowed:
         print("Overflow")
+    x = 0
+    for i in range(1000000):
+        x += 1
     stream.write(chunk)
