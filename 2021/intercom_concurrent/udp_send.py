3950b784a071f1857091d01d05198bb36241a3fc
RaquelGG <raquelgutierrezg.97@gmail.com>
2020-10-21 11:18:01 +0200

    G1: intercom concurrent

---
+++ 2021/intercom_concurrent/udp_send.py
@@ -0,0 +1,18 @@
+import socket
+
+class UdpSender():
+    def __enter__(self):
+        """Create an UDP socket."""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print("socket created")
+        return self
+
+    def send(self, packed_chunk, out_port, destination):
+        """Send data."""
+        self.sock.sendto(packed_chunk, (destination, out_port))
+        #print(f"message {packed_chunk} sent to destination {destination}")
+
+    def __exit__(self,ext_type,exc_value,traceback):
+        """Close the socket."""
+        self.sock.close()
+        print("socket closed")
