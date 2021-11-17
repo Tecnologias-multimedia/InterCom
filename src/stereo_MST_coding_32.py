826b1334a51a8de097d69a7a49bed7d4b4758d10
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-11-17 13:05:52 +0100

    .

--- src/stereo_MST_coding_32.py
+++ src/stereo_MST_coding_32.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         argcomplete.autocomplete(minimal.parser)
     except Exception:
             logging.warning("argcomplete not working :-/")
-            
+
     minimal.args = minimal.parser.parse_known_args()[0]
     if minimal.args.show_stats or minimal.args.show_samples:
         intercom = Stereo_MST_Coding_32__verbose()
