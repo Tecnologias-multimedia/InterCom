8058b86021d42e92fdd8395e7ed773296eac734f
jesuscazorla <jesuscazorlagonzalez01@gmail.com>
2022-01-21 10:40:22 +0100

    Update temporal_overlapped_DWT_coding.py

--- src/temporal_overlapped_DWT_coding.py
+++ src/temporal_overlapped_DWT_coding.py
@@ -1,152 +1,143 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
 
+''' Real-time Audio Intercommunicator (removes intra-channel redundancy with a DWT (Discrete Wavelet Transform)). '''
+
 import numpy as np
 import pywt
-import logging
-import struct
-import zlib
 import math
-
 import minimal
-from temporal_no_overlapped_DWT_coding import Temporal_No_Overlapped_DWT as temp_no_DWT
-from stereo_MST_coding_32 import Stereo_MST_Coding_32 as stereo32
+import logging
 
-class Temporal_Overlapped_DWT(temp_no_DWT):
-    #V1
+from stereo_MST_coding_32 import Stereo_MST_Coding_32 as Stereo_Coding
+
+from temporal_no_overlapped_DWT_coding import Temporal_No_Overlapped_DWT
+from temporal_no_overlapped_DWT_coding import Temporal_No_Overlapped_DWT__verbose
+
+
+class Temporal_Overlapped_DWT(Temporal_No_Overlapped_DWT):
+
     def __init__(self):
         super().__init__()
         logging.info(__doc__)
-        
-        self.overlaped_area_size = self.max_filters_length * (1 << self.dwt_levels)
 
-        self.lista = [] #Encoder
-        for i in range(3):
-            self.lista.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
-            
-                        
-        self.descom = [] #Decoder
-        for i in range(3):
-            self.descom.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
-        
-       
-        #BANDAS PARA 1024 (PERMITE CALCULAR SUBBANDAS)      
-        self.bandas1 = []
-        aux =pywt.wavedecn_shapes((1024,), wavelet= self.wavelet, level=self.dwt_levels, mode='per')
-        
-        self.bandas1.extend(aux[0])
-        for i in range(1,len(aux)):
-            self.bandas1.extend(list(aux[i].values()))
-            self.bandas1[i] = self.bandas1[i][0]
-        
-        #BANDAS EXTENDIDAS (PERMITE CALCULAR SUBBANDAS)
-        self.bandas = []
-        aux =pywt.wavedecn_shapes((1024+2*self.overlaped_area_size,), wavelet=self.wavelet, level=self.dwt_levels, mode='per')
-        
-        self.bandas.extend(aux[0])
-        for i in range(1,len(aux)):
-            self.bandas.extend(list(aux[i].values()))
-            self.bandas[i] = self.bandas[i][0]           
-    
-        
-        #Coeficientes para extendido
-        zero_array = np.zeros(shape=minimal.args.frames_per_chunk+2*self.overlaped_area_size)
+        self.number_of_overlapped_samples = self.max_filters_length * (1 << self.dwt_levels)
+
+        # Structure to keep chunks during encoding
+        self.e_chunk_list = []
+        self.e_chunk_list.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
+        self.e_chunk_list.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
+        self.e_chunk_list.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
+
+        # Structure to keep chunks during decoding
+        self.d_chunk_list = []
+        self.d_chunk_list.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
+        self.d_chunk_list.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
+        self.d_chunk_list.append(np.zeros((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32))
+
+        # Extended slices
+        zero_array = np.zeros(shape=minimal.args.frames_per_chunk+2*self.number_of_overlapped_samples)
         coeffs = pywt.wavedec(zero_array, wavelet=self.wavelet, level=self.dwt_levels, mode="per")
-        self.slices = pywt.coeffs_to_array(coeffs)[1]
-        
+        self.extended_slices = pywt.coeffs_to_array(coeffs)[1]
 
-        
-        
-    def analyze(self,chunk):
-                               
-        self.lista[2] = chunk
-        e = np.concatenate((self.lista[0][-self.overlaped_area_size:], self.lista[1], self.lista[2][:self.overlaped_area_size]))
-        d= self._analyze(e)
-        
-       
-                
-        reduced_d = d[0: self.bandas[0]][self.overlaped_area_size//2**self.dwt_levels : -self.overlaped_area_size//2**self.dwt_levels]
-                    
-        acumulador= self.bandas[0]
+        logging.info(f"number of overlapped samples = {self.number_of_overlapped_samples}")
+        logging.info(f"extended chunk size = {minimal.args.frames_per_chunk+self.number_of_overlapped_samples*2}")
+
+
+    def analyze(self, chunk):
+        fpc = minimal.args.frames_per_chunk
+        o = self.number_of_overlapped_samples
+
+        # Input C_i+1
+        self.e_chunk_list[2] = Stereo_Coding.analyze(self,chunk)
+
+        # Build extended chunk
+        extended_chunk = np.concatenate([self.e_chunk_list[0][-o : ], self.e_chunk_list[1], self.e_chunk_list[2][ : o]])
+
+        # Compute extended decomposition
+        extended_DWT_chunk = self.extended_DWT_encode(extended_chunk)
+
+        # Decomposition subset
+        decomp_subset = np.zeros((0, self.NUMBER_OF_CHANNELS), dtype=np.int32)
+        decomp_subset = np.concatenate(( decomp_subset, extended_DWT_chunk[self.extended_slices[0][0]] [o//2**self.dwt_levels : -o//2**self.dwt_levels] ))
         for i in range(self.dwt_levels):
-            if(i == self.dwt_levels-1):
-                reduced_d = np.concatenate((reduced_d,d[acumulador : acumulador+self.bandas[i+1]][self.overlaped_area_size//2 : -self.overlaped_area_size//2]))
-            else:
-                reduced_d = np.concatenate((reduced_d,d[acumulador : acumulador+self.bandas[i+1]][self.overlaped_area_size//2**(self.dwt_levels-i) : -self.overlaped_area_size//2**(self.dwt_levels-i)]))
-                acumulador += self.bandas[i+1]
-            
-          
-                
-        self.lista[0] = self.lista[1]
-        self.lista[1] = self.lista[2]
-        return reduced_d
+            decomp_subset = np.concatenate(( decomp_subset, extended_DWT_chunk[self.extended_slices[i+1]['d'][0]] [o//2**(self.dwt_levels - i) : -o//2**(self.dwt_levels - i)] ))
 
-    def _analyze(self, chunk):
-        chunk = stereo32.analyze(self,chunk)
-        DWT_chunk = np.zeros((minimal.args.frames_per_chunk+2*self.overlaped_area_size, self.NUMBER_OF_CHANNELS), dtype=np.int32)
+        # C_i-1 <-- C_i
+        self.e_chunk_list[0] = self.e_chunk_list[1]
+        # C_i <-- C_i+1
+        self.e_chunk_list[1] = self.e_chunk_list[2]
+
+        return decomp_subset
+
+    def extended_DWT_encode(self, chunk):
+        DWT_chunk = np.empty((minimal.args.frames_per_chunk + 2*self.number_of_overlapped_samples, self.NUMBER_OF_CHANNELS), dtype=np.int32)
         for c in range(self.NUMBER_OF_CHANNELS):
             channel_coeffs = pywt.wavedec(chunk[:, c], wavelet=self.wavelet, level=self.dwt_levels, mode="per")
             channel_DWT_chunk = pywt.coeffs_to_array(channel_coeffs)[0]
             DWT_chunk[:, c] = channel_DWT_chunk
         return DWT_chunk
-    
-    
+
+
+    # Uses overlapping
     def synthesize(self, chunk_DWT):
-            
-        self.descom[2] = chunk_DWT
-        
-        division = int(self.overlaped_area_size/(2**(self.dwt_levels)))
-        extendido = np.array(self.descom[0][0: self.bandas1[0]][-division: ])
-        acumulado = 0
-        
-        for i in range (self.dwt_levels+1):
-            if(i == 0):
-                extendido = np.concatenate((extendido, self.descom[1][0 : self.bandas1[i]]))
-                extendido = np.concatenate((extendido, self.descom[2][0 : self.bandas1[i]][ : division]))
-                acumulado += self.bandas1[i]
-                
-            elif (i == self.dwt_levels):
-                division = int(self.overlaped_area_size/2)
-                extendido = np.concatenate((extendido,self.descom[0][self.bandas1[i] : ][-division : ]))
-                extendido = np.concatenate((extendido,self.descom[1][self.bandas1[i] : ]))
-                extendido = np.concatenate((extendido,self.descom[2][self.bandas1[i] : ][ : division]))
-            else:
-                division = int(self.overlaped_area_size/(2**(self.dwt_levels-(i-1))))
-                extendido = np.concatenate((extendido,self.descom[0][acumulado : acumulado+self.bandas1[i]][-division : ]))
-                extendido = np.concatenate((extendido,self.descom[1][acumulado : acumulado+self.bandas1[i]]))
-                extendido = np.concatenate((extendido,self.descom[2][acumulado : acumulado+self.bandas1[i]][ : division]))
-                acumulado += self.bandas1[i]
+        o = self.number_of_overlapped_samples
+        fpc = minimal.args.frames_per_chunk
 
-        chunk = self._synthesize(extendido)
-        
+        # Input D_i+1
+        self.d_chunk_list[2] = chunk_DWT
 
-        chunkfinal = chunk[self.overlaped_area_size : -self.overlaped_area_size]
-        
-        self.descom[0] = self.descom[1]
-        self.descom[1] = self.descom[2]
-        
-        return chunkfinal
-      
-    def _synthesize(self, chunk_DWT):
-        chunk = np.zeros((minimal.args.frames_per_chunk+2*self.overlaped_area_size, self.NUMBER_OF_CHANNELS), dtype=np.int32)          
+        # Build extended decomposition
+        extended_DWT_chunk = np.zeros((0, self.NUMBER_OF_CHANNELS), dtype=np.int32)
+        extended_DWT_chunk = np.concatenate(( extended_DWT_chunk, self.d_chunk_list[0] [self.slices[0][0]] [ -o//2**(self.dwt_levels) : ] ))
+        extended_DWT_chunk = np.concatenate(( extended_DWT_chunk, self.d_chunk_list[1] [self.slices[0][0]] ))
+        extended_DWT_chunk = np.concatenate(( extended_DWT_chunk, self.d_chunk_list[2] [self.slices[0][0]] [ : o//2**(self.dwt_levels) ] ))        
+        for i in range(self.dwt_levels):
+            extended_DWT_chunk = np.concatenate(( extended_DWT_chunk, self.d_chunk_list[0] [self.slices[i+1]['d'][0]] [ -o//2**(self.dwt_levels - i) : ] ))
+            extended_DWT_chunk = np.concatenate(( extended_DWT_chunk, self.d_chunk_list[1] [self.slices[i+1]['d'][0]] ))
+            extended_DWT_chunk = np.concatenate(( extended_DWT_chunk, self.d_chunk_list[2] [self.slices[i+1]['d'][0]] [ : o//2**(self.dwt_levels - i) ] ))
+
+        # Compute extended chunk
+        extended_chunk = self.extended_DWT_decode(extended_DWT_chunk)
+
+        # D_i-1 <-- D_i
+        self.d_chunk_list[0] = self.d_chunk_list[1]
+        # D_i <-- D_i+1
+        self.d_chunk_list[1] = self.d_chunk_list[2]
+
+        return Stereo_Coding.synthesize(self,extended_chunk[o:o+fpc])
+
+    def extended_DWT_decode(self, chunk_DWT):
+        chunk = np.empty((minimal.args.frames_per_chunk + 2*self.number_of_overlapped_samples, self.NUMBER_OF_CHANNELS), dtype=np.int32)
+        for c in range(self.NUMBER_OF_CHANNELS):
+            channel_coeffs = pywt.array_to_coeffs(chunk_DWT[:, c], self.extended_slices, output_format="wavedec")
+            chunk[:, c] = pywt.waverec(channel_coeffs, wavelet=self.wavelet, mode="per")
+        return chunk
+
+
+    '''
+    # Ignores overlapping
+    def synthesize(self, chunk_DWT):
+        chunk = np.empty((minimal.args.frames_per_chunk, self.NUMBER_OF_CHANNELS), dtype=np.int32)
         for c in range(self.NUMBER_OF_CHANNELS):
             channel_coeffs = pywt.array_to_coeffs(chunk_DWT[:, c], self.slices, output_format="wavedec")
-            #chunk[:, c] = np.rint(pywt.waverec(channel_coeffs, wavelet=self.wavelet, mode="per")).astype(np.int32)
             chunk[:, c] = pywt.waverec(channel_coeffs, wavelet=self.wavelet, mode="per")
-        chunk= stereo32.synthesize(self,chunk)
-        return chunk   
-    
-from temporal_no_overlapped_DWT_coding import Temporal_No_Overlapped_DWT__verbose as temp_no_DWT__verbose
-    
-   
-class Temporal_Overlapped_DWT__verbose(Temporal_Overlapped_DWT,temp_no_DWT__verbose):
-     def compute(self, indata, outdata):
+        chunk = Stereo_Coding.synthesize(self,chunk)
+        return chunk
+    '''
+
+
+class Temporal_Overlapped_DWT__verbose(Temporal_Overlapped_DWT, Temporal_No_Overlapped_DWT__verbose):
+
+    # Modified so the added chunk delay is taken into account
+    def compute(self, indata, outdata):
         # Remember that indata contains the recorded chunk and
         # outdata, the played chunk, but this is only true after
         # running this method.
-        
+
         self.recorded_chunks_buff[self.chunk_number % self.cells_in_buffer] = indata.copy()
-        recorded_chunk = self.recorded_chunks_buff[(self.chunk_number - self.chunks_to_buffer - 3) % (self.cells_in_buffer)].astype(np.double) #CAMBIO PARA DELAY
+        #recorded_chunk = self.recorded_chunks_buff[(self.chunk_number - self.chunks_to_buffer - 1) % (self.cells_in_buffer)].astype(np.double)
+        recorded_chunk = self.recorded_chunks_buff[(self.chunk_number - self.chunks_to_buffer - 3) % (self.cells_in_buffer)].astype(np.double)  # <- Modification
         played_chunk = outdata.astype(np.double)
 
         if minimal.args.show_samples:
@@ -155,7 +146,7 @@ class Temporal_Overlapped_DWT__verbose(Temporal_Overlapped_DWT,temp_no_DWT__verb
             # Remember that
             # buffer.Buffering__verbose._record_io_and_play shows also
             # indata and outdata.
-        
+
             print("\033[32mbr_control: ", end=''); self.show_outdata(played_chunk.astype(np.int))
             print("\033[m", end='')
 
@@ -175,20 +166,20 @@ class Temporal_Overlapped_DWT__verbose(Temporal_Overlapped_DWT,temp_no_DWT__verb
         # multiplication "numpy.multiply(a, b)" that when "a" and "b"
         # are equal, generates the same result. Among all these
         # alternatives, the dot product seems to be the faster one.
-       
+
         signal_energy = [None] * self.NUMBER_OF_CHANNELS
         for c in range(self.NUMBER_OF_CHANNELS):
             signal_energy[c] = np.sum( square_signal[c] )
- 
+
         # Compute distortions
         error_signal = [None] * self.NUMBER_OF_CHANNELS
         for c in range(self.NUMBER_OF_CHANNELS):
             error_signal[c] = recorded_chunk[:, c] - played_chunk[:, c]
-            
+
         square_error_signal = [None] * self.NUMBER_OF_CHANNELS
         for c in range(self.NUMBER_OF_CHANNELS):
             square_error_signal[c] = error_signal[c] * error_signal[c]
-            
+
         error_energy = [None] * self.NUMBER_OF_CHANNELS
         for c in range(self.NUMBER_OF_CHANNELS):
             error_energy[c] = np.sum( square_error_signal[c] )
