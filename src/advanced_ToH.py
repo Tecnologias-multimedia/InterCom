722f1fefa3236e6ccb9ae5df678d1bc5542ae094
Tony David Sprotte <tony.sprotte@gmx.de>
2023-01-09 18:16:45 +0100

    fix: false inheritance

--- src/advanced_ToH.py
+++ src/advanced_ToH.py
@@ -19,37 +19,42 @@ class AdvancedTreshhold(Treshold):
         super().__init__()
         logging.info(__doc__)
 
-    def analyze_hamming_fft(chunk, size):
+    def analyze_hamming_fft(self, chunk, size):
         hamming_window = np.hamming(size)
-        return np.fft.fft(chunk / hamming_window)
+        chunk[:, 0] = (chunk[:, 0] / hamming_window).astype(np.int32)
+        chunk[:, 1] = (chunk[:, 1] / hamming_window).astype(np.int32)
+        return np.fft.fft(chunk)
 
     def analyze(self, chunk):
-        chunk_DWT = Temporal_Overlapped_DWT().analyze(chunk)
+        chunk_DWT = Temporal_Overlapped_DWT.analyze(self, chunk)
 
         # Quantize the subbands
         chunk_DWT[self.slices[0][0]] = (self.analyze_hamming_fft(
-            chunk_DWT[self.slices[0][0]], self.slices[0][0]) / self.quantization_steps[0]).astype(np.int32)
+            chunk_DWT[self.slices[0][0]], chunk_DWT[self.slices[0][0]].shape[0]) / self.quantization_steps[0]).astype(np.int32)
         for i in range(self.dwt_levels):
             chunk_DWT[self.slices[i+1]['d'][0]] = (
                 self.analyze_hamming_fft(
-                    chunk_DWT[self.slices[0][0]], self.slices[i+1]['d'][0]) / self.quantization_steps[i+1]).astype(np.int32)
+                    chunk_DWT[self.slices[i+1]['d'][0]], chunk_DWT[self.slices[i+1]['d'][0]].shape[0]) / self.quantization_steps[i+1]).astype(np.int32)
 
         return chunk_DWT
 
-    def synthesize_hamming_fft(chunk, size):
+    def synthesize_hamming_fft(self, chunk, size):
         hamming_window = np.hamming(size)
-        return np.fft.fft(chunk) * hamming_window
+        chunk = np.fft.ifft(chunk)
+        chunk[:, 0] = (chunk[:, 0] * hamming_window).astype(np.int32)
+        chunk[:, 1] = (chunk[:, 1] * hamming_window).astype(np.int32)
+        return chunk
 
     def synthesize(self, chunk_DWT):
 
         # Dequantize the subbands
         chunk_DWT[self.slices[0][0]] = self.synthesize_hamming_fft(
-            chunk_DWT[self.slices[0][0]], self.slices[0][0]) * self.quantization_steps[0]
+            chunk_DWT[self.slices[0][0]], chunk_DWT[self.slices[0][0]].shape[0]) * self.quantization_steps[0]
         for i in range(self.dwt_levels):
             chunk_DWT[self.slices[i+1]['d'][0]] = self.synthesize_hamming_fft(
-                chunk_DWT[self.slices[i+1]['d'][0]], self.slices[i+1]['d'][0]) * self.quantization_steps[i+1]
+                chunk_DWT[self.slices[i+1]['d'][0]], chunk_DWT[self.slices[i+1]['d'][0]].shape[0]) * self.quantization_steps[i+1]
 
-        return Temporal_Overlapped_DWT().synthesize(chunk_DWT)
+        return Temporal_Overlapped_DWT.synthesize(self, chunk_DWT)
 
 
 class AdvancedTreshold__verbose(AdvancedTreshhold, Treshold__verbose):
@@ -69,9 +74,9 @@ if __name__ == "__main__":
         logging.warning("argcomplete not working :-/")
     minimal.args = minimal.parser.parse_known_args()[0]
     if minimal.args.show_stats or minimal.args.show_samples:
-        intercom = Treshold__verbose()
+        intercom = AdvancedTreshold__verbose()
     else:
-        intercom = Treshold()
+        intercom = AdvancedTreshhold()
     try:
         intercom.run()
     except KeyboardInterrupt:
