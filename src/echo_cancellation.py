a5641e549265b926a81fa4ff797d5166f3db1dd5
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2024-09-30 21:56:34 +0200

    .

--- src/echo_cancellation.py
+++ src/echo_cancellation.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         logging.warning("argcomplete not working :-/")
     minimal.args = minimal.parser.parse_known_args()[0]
 
-    if minimal.args.show_stats or minimal.args.show_samples:
+    if minimal.args.show_stats or minimal.args.show_samples or minimal.args.show_spectrum:
         intercom = Echo_Cancellation__verbose()
     else:
         intercom = Echo_Cancellation()
