d49620ffc8f0a21e7a6e232c94ff72f88549522b
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2024-12-29 06:59:58 +0100

    .

--- src/echo_cancellation.py
+++ src/echo_cancellation.py
@@ -1,21 +1,99 @@
 #!/usr/bin/env python
 # PYTHON_ARGCOMPLETE_OK
 
-'''Echo cancellation (template).'''
+'''Echo cancellation LMS.'''
 
 import logging
-
 import minimal
 import buffer
-        
+import numpy as np
+from adaptfilt import nlms
+
 class Echo_Cancellation(buffer.Buffering):
     def __init__(self):
         super().__init__()
         logging.info(__doc__)
+
+    def cancel_echo_old(self, microphone_signal, speaker_signal):
+        M = 256
+        step = 0.000001
+        y = np.zeros((1024-M+1,2))
+        e = np.zeros((1024-M+1,2))
+
+        for i in range(microphone_signal.shape[1]):
+            y[:, i], e[:, i], w = nlms(speaker_signal[:, i], microphone_signal[:, i], M, step)
+
+        return e
         
+    def cancel_echo_old(self, microphone_signal, speaker_signal):
+        M = 256
+        step = 0.000001
+        y = np.zeros((1024-M+1,2))
+        e = np.zeros((1024-M+1,2))
+        #print(np.max(microphone_signal))
+        print(np.max(speaker_signal))
+
+        for i in range(microphone_signal.shape[1]):
+            y[:, i], e[:, i], w = nlms(speaker_signal[:, i].astype(np.float32), microphone_signal[:, i].astype(np.float32), M, step)
+
+        return y
+
+    def cancel_echo(self, microphone_signal, speaker_signal):
+        M = 256
+        step = 0.1
+        padding = np.zeros((M-1, 2))
+        padded_SP = np.vstack([padding, speaker_signal])
+        padded_MS = np.vstack([padding, microphone_signal])
+        y = np.zeros((1024,2))
+        e = np.zeros((1024,2))
+        #print(np.max(microphone_signal))
+        #print(np.max(speaker_signal))
+
+        for i in range(microphone_signal.shape[1]):
+            y[:, i], e[:, i], w = nlms(padded_SP[:, i].astype(np.float32), padded_MS[:, i].astype(np.float32), M, step)
+
+        return y
+        
+    def cancel_echo_old(self, microphone_signal, speaker_signal):
+        M = 256
+        step = 0.1
+        padding = np.zeros((M-1, 2))
+        padded_SP = np.vstack([padding, speaker_signal])
+        padded_MS = np.vstack([padding, microphone_signal])
+        y = np.zeros((1024,2))
+        e = np.zeros((1024,2))
+        #print(np.max(microphone_signal))
+        #print(np.max(speaker_signal))
+
+        for i in range(microphone_signal.shape[1]):
+            y[:, i], e[:, i], w = nlms(padded_SP[:, i], padded_MS[:, i], M, step)
+
+        return y
+
+    def _record_IO_and_play_old(self, indata, outdata, frames, time, status):
+        echo_signal = self.cancel_echo(indata, outdata)
+        padding = np.zeros((1024 - echo_signal.shape[0], 2))
+        echo_signal = np.vstack([echo_signal, padding])
+
+        indata[:] = echo_signal
+        super()._record_IO_and_play(indata, outdata, frames, time, status)
+
+    def _record_IO_and_play(self, indata, outdata, frames, time, status):
+        ECS = self.cancel_echo(indata, outdata)
+        #print(np.max(ECS))
+        padding = np.zeros((1024 - ECS.shape[0], 2))
+        ECS = np.vstack([ECS, padding])
+        #indata[:] = ECS
+        super()._record_IO_and_play(indata, outdata, frames, time, status)
+
+    def _record_IO_and_play(self, indata, outdata, frames, time, status):
+        ECS = self.cancel_echo(indata, outdata)
+        super()._record_IO_and_play(indata, outdata, frames, time, status)
+
 class Echo_Cancellation__verbose(Echo_Cancellation, buffer.Buffering__verbose):
     def __init__(self):
         super().__init__()
+
 
 try:
     import argcomplete  # <tab> completion for argparse.
@@ -39,4 +117,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         minimal.parser.exit("\nSIGINT received")
     finally:
-       intercom.print_final_averages()
+        intercom.print_final_averages()
